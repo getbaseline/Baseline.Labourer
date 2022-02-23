da97f2e5a6e60d0e1868e239bc9354630bc95197
Liam Symonds <liam@liamsymonds.com>
2022-02-23 21:36:28 +0000

    add docblocks

--- src/Baseline.Labourer.AspNetCore/BaselineLabourerHostedService.cs
+++ src/Baseline.Labourer.AspNetCore/BaselineLabourerHostedService.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Hosting;
 
 namespace Baseline.Labourer
 {
+    /// <summary>
+    /// A hosted service used to run the Baseline.Labourer server.
+    /// </summary>
     public class BaselineLabourerHostedService : IHostedService
     {
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
@@ -17,11 +20,19 @@ namespace Baseline.Labourer
             _labourerServer = serviceProvider.GetService<LabourerServer>()!;
         }
 
+        /// <summary>
+        /// Runs the server, booting all of the relevant workers to process jobs, dispatch scheduled jobs that need to
+        /// run and more.
+        /// </summary>
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             await _labourerServer.RunServerAsync();
         }
 
+        /// <summary>
+        /// Cancels the cancellation token source used within the Baseline.Labourer server which should result in the
+        /// graceful shutdown of all relevant services.
+        /// </summary>
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _cancellationTokenSource.Cancel();
