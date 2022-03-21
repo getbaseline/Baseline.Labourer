7dff112620fc34fc0991ae1609bed083d609f863
Liam Symonds <liam@liamsymonds.com>
2022-03-21 22:16:29 +0000

    add test projects

---
+++ test/Baseline.Labourer.Queue.Sqlite.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Baseline.Labourer.Queue.Sqlite.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ test/Baseline.Labourer.Store.Sqlite.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Baseline.Labourer.Store.Sqlite.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
