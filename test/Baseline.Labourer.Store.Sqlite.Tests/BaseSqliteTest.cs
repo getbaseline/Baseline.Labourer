0f978f859e4f3caf10811ef0689ae020f48a36f4
Liam Symonds <liam@liamsymonds.com>
2022-03-22 21:25:55 +0000

    wip

---
+++ test/Baseline.Labourer.Store.Sqlite.Tests/BaseSqliteTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Baseline.Labourer.Store.Sqlite.Tests;
+
+public abstract class BaseSqliteTest : IDisposable
+{
+    private readonly Guid _databaseId = Guid.NewGuid();
+    protected readonly string ConnectionString;
+    protected readonly SqliteConnection Connection;
+
+    protected BaseSqliteTest()
+    {
+        ConnectionString = $"Data Source={_databaseId};Mode=Memory;Cache=Shared";
+        Connection = new SqliteConnection(ConnectionString);
+        Connection.Open();
+    }
+    
+    public void Dispose()
+    {
+        Connection.Dispose();
+    }
+}
--- test/Baseline.Labourer.Store.Sqlite.Tests/SqliteStoreTests.cs
+++ test/Baseline.Labourer.Store.Sqlite.Tests/SqliteStoreTests.cs
@@ -6,35 +6,19 @@ using Xunit;
 
 namespace Baseline.Labourer.Store.Sqlite.Tests;
 
-public class SqliteStoreTests : IDisposable
+public class SqliteStoreTests : BaseSqliteTest
 {
-    private readonly Guid _databaseId = Guid.NewGuid();
-    private readonly string _connectionString;
-    private readonly SqliteConnection _connection;
-
-    public SqliteStoreTests()
-    {
-        _connectionString = $"Data Source={_databaseId};Mode=Memory;Cache=Shared";
-        _connection = new SqliteConnection(_connectionString);
-        _connection.Open();
-    }
-
-    public void Dispose()
-    {
-        _connection.Dispose();
-    }
-        
     [Fact]
     public async Task It_Creates_The_Relevant_Tables_And_Migrates_The_Migrations()
     {
         // Arrange.
-        var store = new SqliteStore(_connectionString);
+        var store = new SqliteStore(ConnectionString);
 
         // Act.
         await store.BootstrapAsync();
         
         // Assert.
-        var entriesInVersionTable = (long)new SqliteCommand("SELECT count(1) FROM bl_lb_version_history", _connection)
+        var entriesInVersionTable = (long)new SqliteCommand("SELECT count(1) FROM bl_lb_version_history", Connection)
             .ExecuteScalar()!;
         
         entriesInVersionTable.Should().Be(1);
@@ -44,14 +28,14 @@ public class SqliteStoreTests : IDisposable
     public async Task It_Does_Not_Run_The_Same_Migrations_Twice()
     {
         // Arrange.
-        var store = new SqliteStore(_connectionString);
+        var store = new SqliteStore(ConnectionString);
 
         // Act.
         await store.BootstrapAsync();
         await store.BootstrapAsync();
         
         // Assert.
-        var entriesInVersionTable = (long)new SqliteCommand("SELECT count(1) FROM bl_lb_version_history", _connection)
+        var entriesInVersionTable = (long)new SqliteCommand("SELECT count(1) FROM bl_lb_version_history", Connection)
             .ExecuteScalar()!;
         
         entriesInVersionTable.Should().Be(1);
